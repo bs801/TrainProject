91b9ca566ba05c6e03dd3c15647fc304d78d35bf
PHIL528 <philippaulmurray@gmail.com>
2020-12-12 16:20:02 +0100

    User update

--- src/com/cs336/pkg/User.java
+++ src/com/cs336/pkg/User.java
@@ -1,5 +1,34 @@
 package com.cs336.pkg;
 
-public class User {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+/*
+ * 	username varchar(20) primary key,
+    password varchar(20) NOT NULL,
+    
+    firstName varchar(20),
+    lastName varchar(20),
+    emailAddress varchar(20)
+ * 
+ * 
+ */
+
+public class User {
+	
+	String username;
+	String password;
+	String firstName;
+	String lastName;
+	String email;
+	
+	public User(String username, String password, String firstName, String lastName, String email) {
+		this.username = username; this.password = password; this.firstName = firstName; this.lastName = lastName; this.email = email;
+	}
+	
+	public User(ResultSet rs) throws SQLException {
+		this(rs.getString("username"), rs.getString("password"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"));		
+	}
+	
+	
 }
