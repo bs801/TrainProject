9970b69a3c04ae911823d700e678b66211f7106d
Bohan Song <bs801@scarletmail.rutgers.edu>
2020-12-12 10:34:16 -0500

    changes

--- src/com/cs336/pkg/Representative.java
+++ src/com/cs336/pkg/Representative.java
@@ -1,5 +1,8 @@
 package com.cs336.pkg;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /*
  * 
  * 
@@ -22,6 +25,17 @@ public class Representative {
 	String firstName;
 	String lastName;
 	
+	public Representative(String username, String password, String firstName, String lastName, String SSN) {
+		this.username = username; this.password = password; this.firstName = firstName; this.lastName = lastName; this.SSN = SSN;
+	}
 	
+	public Representative(ResultSet rs) throws SQLException {
+		this(rs.getString("username"), rs.getString("password"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("SSN"));		
+	}
+	
+	public String toString() {
+		return firstName + " " + lastName; 
+		
+	}
 	
 }
